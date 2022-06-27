e6a91688342e949958375795955176d4254a310c
fatpound <84704675+fatpound@users.noreply.github.com>
2022-06-27 14:03:13 +0300

    Update 007 - 10001st prime.c

--- Project Euler/007 - 10001st prime.c
+++ Project Euler/007 - 10001st prime.c
@@ -6,9 +6,9 @@ int IsPrime(int n)
 	if (n < 4) return (n > 1);
 	if (n % 2 == 0 || n % 3 == 0) return 0;
 	
-	int i, j = n + 1;
+	int i;
 	
-	for (i = 5; i * i < j; i += 6)
+	for (i = 5; i * i < n + 1; i += 6)
 	{
 		if (n % i == 0 || n % (i + 2) == 0)
 		{
