f7dfa0b620afd79487bd02123224066aa66bd72a
fatpound <84704675+fatpound@users.noreply.github.com>
2022-06-27 15:02:41 +0300

    Add files via upload

---
+++ Project Euler/006 - Sum square difference.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int a, b = 0, c = 0;
+	
+	for (a = 1; a < 11; a++)
+	{
+		b += a * a;
+		c += a;
+		
+		if (a == 10)
+		{
+			c = c * c;
+		}
+	}
+	
+	printf("%d", c - b);
+	
+	getch();
+	return 0;
+}
