72be00318ad3d890ef8331114f2669be3f104d7b
fatpound <84704675+fatpound@users.noreply.github.com>
2022-06-30 16:45:04 +0300

    Update FOR 02.c

--- Selçuk Bilişim C/FOR/FOR 02.c
+++ Selçuk Bilişim C/FOR/FOR 02.c
@@ -5,7 +5,7 @@
 
 0’dan n’e kadar(n bilgisi kullanıcıdan alınacak) olan sayıların kullanıcının girdiği iki sayıya tam bölünebilme durumunu ekrana yazan program.
 
-Örn: Kullanıcı 50,4 ve 11 sayılarını girdi, 0’dan 50’ye kadar olan sayıların 4’e bölünebilme,11’e bölünebilme, hem 4’e hem 11’e bölünebilme durumları ekrana yazdırılacak.
+Örn: Kullanıcı 50, 4 ve 11 sayılarını girdi, 0’dan 50’ye kadar olan sayıların 4’e bölünebilme,11’e bölünebilme, hem 4’e hem 11’e bölünebilme durumları ekrana yazdırılacak.
 
 */
 
@@ -16,7 +16,7 @@ int main()
 	printf("lutfen uc sayi giriniz...\n");
 	scanf("%d%d%d", &n, &b, &c);
 	
-	for (a = 1; a < n; a++)
+	for (a = 1; a < n; a++) // a <= n de yazılabilir
 	{
 		if (a % b == 0 && a % c == 0)
 		{
