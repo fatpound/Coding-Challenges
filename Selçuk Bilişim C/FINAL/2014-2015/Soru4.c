a9036f2775a7bdc74bfbff3e0ef6ff22fc8efddb
fatpound <84704675+fatpound@users.noreply.github.com>
2022-07-01 17:20:14 +0300

    Create Soru4.c

---
+++ Selçuk Bilişim C/FINAL/2014-2015/Soru4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	unsigned int i, x, y = 0b00001111000011110000111100001111;
+	
+	printf("lutfen bir tamsayi giriniz > ");
+	scanf("%u", &x);
+
+	for (i = 1U << 31; i; i >>= 1)
+	{
+		printf(x & i ? "1" : "0");
+	}
+
+	printf("\n");
+
+	y |= x;
+
+	for (i = 1U << 31; i; i >>= 1)
+	{
+		printf(y & i ? "1" : "0");
+	}
+
+	getch();
+	return 0;
+}
