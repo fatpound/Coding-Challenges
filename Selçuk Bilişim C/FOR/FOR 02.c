33f13c376c9f4762eee339984018f92172d2578c
fatpound <84704675+fatpound@users.noreply.github.com>
2022-07-02 18:04:31 +0300

    Update FOR 02.c

--- Selçuk Bilişim C/FOR/FOR 02.c
+++ Selçuk Bilişim C/FOR/FOR 02.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 #include <conio.h>
 
-/*
-
-0’dan n’e kadar(n bilgisi kullanıcıdan alınacak) olan sayıların kullanıcının girdiği iki sayıya tam bölünebilme durumunu ekrana yazan program.
-
-Örn: Kullanıcı 50, 4 ve 11 sayılarını girdi, 0’dan 50’ye kadar olan sayıların 4’e bölünebilme,11’e bölünebilme, hem 4’e hem 11’e bölünebilme durumları ekrana yazdırılacak.
-
-*/
-
 int main()
 {
 	int a, b, c, n;
