93cf40863e795d1190da15299b7bcf65e40f706b
fatpound <84704675+fatpound@users.noreply.github.com>
2022-09-28 23:06:51 +0300

    Add files via upload

---
+++ Sayılar Atlası/LuckyNumbers.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+#define SWAP(x, y) (x == y ? 0 : (x ^= y ^= x ^= y))
+
+void RotateZeros(int *d, int n)
+{
+	int i, j;
+	
+	for (i = 0; i < n; i++)
+	{
+		if (!d[i])
+		{
+			for (j = i; j < n - 1; j++)
+			{
+				SWAP(d[j], d[j + 1]);
+			}
+		}
+	}
+}
+
+int main()
+{
+	int x = 100;
+	
+	int i, j, k = 1, d[x];
+	
+	for (i = 0; i < x; i++)
+	{
+		d[i] = i * 2 + 1;
+	}
+	
+	while ((i = d[k]) && (d[k] * 2 < x))
+	{
+		for (j = i; j < x; j += i)
+		{
+			d[j - 1] = 0;
+		}
+		
+		k++;
+		RotateZeros(d, x);
+	}
+	
+	for (i = 0; i < x; d[i] ? printf("%d ", d[i]) : (i = x), i++);
+	
+	getch();
+	return 0;
+}
