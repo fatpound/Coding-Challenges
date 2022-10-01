4bb7ccce16b008ff2dc1e4311f13285c7a38e23d
fatpound <84704675+fatpound@users.noreply.github.com>
2022-10-01 09:16:07 +0300

    Update SabitBinKurraNumbers.c

--- Sayılar Atlası/SabitBinKurraNumbers.c
+++ Sayılar Atlası/SabitBinKurraNumbers.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #include <conio.h>
 
-int Power(int x, int n)
-{
-	return (n > 0 ? (Power(x, n - 1) * x) : 1);
-}
-
 int main()
 {
 	int n;
 	
 	for (n = 0; n < 26; n++)
 	{
-		printf("%d\n", 3 * Power(2, n) - 1);
+		printf("%d\n", 3 * (1 << n) - 1);
 	}
 	
 	getch();
