f533f909dceefbe5fb9ca1ea7db2f0e5da094f6c
fatpound <84704675+fatpound@users.noreply.github.com>
2022-10-20 18:59:18 +0300

    Add files via upload

---
+++ Project Euler/016 - Power digit sum.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+	int i, j, prod, count, sum = 0, eldevar;
+	char *p = NULL;
+	
+	for (count = (int)floor(1000 * log10(2) + 1), p = (char*) malloc(sizeof(int) * (count + 1)), i = 0; i < count; p[i++] = '0');
+	for (p[i] = 0, p[i - 1] = '2', i = 2; i <= 1000; i++)
+	{
+		for (eldevar = prod = 0, j = count - 1; j >= 0; j--)
+		{
+			prod = (p[j] - 48) * 2 + eldevar;
+			p[j] = (prod % 10) + 48;
+			eldevar = prod / 10;
+		}
+	}
+	
+	// printf("%s", p);
+	
+	for (i = 0; i < count; i++)
+	{
+		sum += (p[i] - 48);
+	}
+	
+	printf("%d", sum);
+	
+	return (!getch());
+}
