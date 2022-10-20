02670dbc5327cb56c7a876d22ddb79fbd376e718
fatpound <84704675+fatpound@users.noreply.github.com>
2022-10-20 19:14:17 +0300

    Update 016 - Power digit sum.c

--- Project Euler/016 - Power digit sum.c
+++ Project Euler/016 - Power digit sum.c
@@ -8,7 +8,7 @@ int main()
 	int i, j, prod, count, sum = 0, eldevar;
 	char *p = NULL;
 	
-	for (count = (int)floor(1000 * log10(2) + 1), p = (char*) malloc(sizeof(int) * (count + 1)), i = 0; i < count; p[i++] = '0');
+	for (count = (int)floor(1000 * log10(2) + 1), p = (char*) malloc(sizeof(char) * (count + 1)), i = 0; i < count; p[i++] = '0');
 	for (p[i] = 0, p[i - 1] = '2', i = 2; i <= 1000; i++)
 	{
 		for (eldevar = prod = 0, j = count - 1; j >= 0; j--)
