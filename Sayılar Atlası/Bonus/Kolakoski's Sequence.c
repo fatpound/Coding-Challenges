8df6209d1aa60ac52e8ca1d9396b72d1a7fb58bf
fatpound <84704675+fatpound@users.noreply.github.com>
2022-10-22 13:18:59 +0300

    Add files via upload

---
+++ Sayılar Atlası/Bonus/Kolakoski's Sequence.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+#define ARR_LEN 17
+
+int main()
+{
+	int i, j, k = 0, x, n = 2, count = 1, *d = NULL;
+	
+	d = (int*) malloc(sizeof(int) * count);
+	
+	for (d[0] = x = i = 1; k < ARR_LEN; x++)
+	{
+		for (j = 0; j < (k ? d[x] : n); j++)
+		{
+			d = (int*)realloc(d, sizeof(int) * ++count);
+			d[i + k++] = n;
+		}
+		
+		n = (d[i + --k] == 2 ? 1 : 2);
+		k++;
+	}
+	
+	for (i = 0; i < ARR_LEN; printf("%d ", d[i++]));
+	
+	return (!getch());
+}
