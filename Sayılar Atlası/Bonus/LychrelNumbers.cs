8d1ac2d1ce6f2a58e6454ce91145edd4b970578e
fatpound <84704675+fatpound@users.noreply.github.com>
2022-11-07 15:33:12 +0300

    Create LychrelNumbers.cs

---
+++ Sayılar Atlası/Bonus/LychrelNumbers.cs
@@ -0,0 +1,61 @@
+using System.Numerics;
+
+namespace SayilarAtlasi
+{
+    internal class LychrelNumbers
+    {
+        static int DigitCount(BigInteger n)
+        {
+            return (n > 0 ? (1 + DigitCount(n / 10)) : 0);
+        }
+
+        static BigInteger RevNum(BigInteger n)
+        {
+            return (n != 0 ? (n % 10) * (BigInteger.Pow(10, DigitCount(n) - 1)) + RevNum(n / 10) : 0);
+        }
+
+        static int IsPalindrome(BigInteger n)
+        {
+            BigInteger x = n;
+            int i, count = DigitCount(n);
+
+            BigInteger[] str = new BigInteger[count + 1];
+
+            for (i = 0; i < count; str[i++] = x % 10, x /= 10);
+            for (i = 0; i < count; i++)
+            {
+                if (str[i] != str[count - i - 1])
+                {
+                    return 0;
+                }
+            }
+
+            return 1;
+        }
+        static int IsLychrel(BigInteger n)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                if (IsPalindrome(n += RevNum(n)) == 1)
+                {
+                    return 0;
+                }
+            }
+
+            return 1;
+        }
+
+        public static int Main(string[] args)
+        {
+            for (BigInteger n = BigInteger.One; n < 10000; n++)
+            {
+                if (IsLychrel(n) == 1)
+                {
+                    Console.WriteLine(n);
+                }
+            }
+            
+            return 0;
+        }
+    }
+}
