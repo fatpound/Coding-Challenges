3bd98c458d3449291f9046f156b53566394c0472
fatpound <84704675+fatpound@users.noreply.github.com>
2022-12-06 16:52:36 +0300

    Create 05 - Simple Math.java

---
+++ Exercises for Programmers/05 - Simple Math.java
@@ -0,0 +1,56 @@
+package s5;
+
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner   scanf = new Scanner(System.in);
+        Integer[] num = new Integer[2];
+        
+        for (int i = 0; i < 2; i++)
+        {
+            System.out.printf("What is the %s number? ", i == 0 ? "first" : "second");
+            num[i] = Integer.valueOf(scanf.next());
+        }
+        
+        System.out.println("");
+        String operation = "+-*/";
+        
+        for (int i = 0; i < 4; i++)
+        {
+            System.out.print(num[0] + " " + operation.charAt(i) + " " + num[1] + " = ");
+            
+            switch (i)
+            {
+                case 0:
+                {
+                    System.out.println(num[0] + num[1]);
+                    break;
+                }
+                
+                case 1:
+                {
+                    System.out.println(num[0] - num[1]);
+                    break;
+                }
+                
+                case 2:
+                {
+                    System.out.println(num[0] * num[1]);
+                    break;
+                }
+                
+                case 3:
+                {
+                    System.out.printf("%.0f\n", (float)num[0] / num[1]);
+                    break;
+                }
+                
+                default:
+                    break;
+            }
+        }
+    }
+}
