1a280fb0f99ab418e4a964143614fffc260fae36
fatpound <84704675+fatpound@users.noreply.github.com>
2022-12-07 10:40:17 +0300

    Update 7.6 - Smith Numbers.java

--- Programming Challenges/7.6 - Smith Numbers.java
+++ Programming Challenges/7.6 - Smith Numbers.java
@@ -9,17 +9,17 @@ public class PC
         if (n < 4)
             return (n > 1);
         
-	      if (n % 2 == 0 || n % 3 == 0)
+        if (n % 2 == 0 || n % 3 == 0)
             return false;
-	
-	      for (int i = 5; i * i <= n; i += 6)
-	      {
+        
+        for (int i = 5; i * i <= n; i += 6)
+        {
             if (n % i == 0 || n % (i + 2) == 0)
             {
                 return false;
             }
-	      }
-	
+        }
+        
         return true;
     }
     
