3bc5f291936b4ce593c3dec6a4e42b822075202c
fatpound <84704675+fatpound@users.noreply.github.com>
2022-12-07 23:11:54 +0300

    Create 5.1 - Primary Arithmetic.java

---
+++ Programming Challenges/5.1 - Primary Arithmetic.java
@@ -0,0 +1,48 @@
+package pc;
+
+import java.util.*;
+
+public class Main
+{
+    public static int DigitCount(int n)
+    {
+        return (n > 0 ? (1 + DigitCount(n / 10)) : 0);
+    }
+    
+    public static void main(String[] args)
+    {
+        Scanner scanf = new Scanner(System.in);
+        
+        int a  = scanf.nextInt(), b  = scanf.nextInt();
+        int ad = DigitCount(a), bd = DigitCount(b);
+        
+        int carry = 0, count = 0;
+        
+        for (int i = 0; i < Math.max(ad, bd); i++)
+        {
+            int sum = a % 10 + b % 10 + carry;
+            
+            if (sum > 9)
+            {
+                count++;
+                carry = sum % 10;
+            }
+            
+            a /= 10;
+            b /= 10;
+        }
+        
+        if (count == 0)
+        {
+            System.out.println("No carry operation.");
+        }
+        else if (count == 1)
+        {
+            System.out.println("1 carry operation.");
+        }
+        else
+        {
+            System.out.println(count + " carry operations.");
+        }
+    }
+}
