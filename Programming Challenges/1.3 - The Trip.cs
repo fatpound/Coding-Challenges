2a85b76c66cb199b7802d8e43f065488f5a66738
fatpound <84704675+fatpound@users.noreply.github.com>
2022-12-08 21:07:35 +0300

    Update 1.3 - The Trip.cs

--- Programming Challenges/1.3 - The Trip.cs
+++ Programming Challenges/1.3 - The Trip.cs
@@ -24,7 +24,30 @@ internal class PC
             exchange += MathF.Abs(expenses[i] - average);
         }
 
-        return exchange / 2 - ((average - (int)average) < 0.01f ? 0.01f : 0f);
+        exchange /= 2;
+
+        float temp = average - (int)average;
+
+        while (temp > 0.01f)
+        {
+            temp -= 0.01f;
+        }
+
+        if (temp != 0)
+        {
+            float shift = average * 100;
+
+            if ((shift - (int)shift) < 1)
+            {
+                exchange -= 0.01f;
+            }
+        }
+
+        exchange *= 100;
+        exchange  = (float)((int)exchange);
+        exchange /= 100;
+
+        return exchange;
     }
 
     static int Main(string[] args)
