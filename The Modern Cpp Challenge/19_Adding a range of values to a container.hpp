cf39140806c97fa2aec3296d2af190269dada84f
fatpound <84704675+fatpound@users.noreply.github.com>
2024-02-19 22:04:11 +0300

    Update 19_Adding a range of values to a container.hpp

--- The Modern Cpp Challenge/19_Adding a range of values to a container.hpp
+++ The Modern Cpp Challenge/19_Adding a range of values to a container.hpp
@@ -9,7 +9,7 @@ namespace tmcc::q19
     template <class C, typename T>
     concept Extensible = requires(C c, T t)
     {
-        { c.push_back(t) };
+        c.push_back(t);
     };
 
     template <typename E, typename... T> requires Extensible<E, T...>
